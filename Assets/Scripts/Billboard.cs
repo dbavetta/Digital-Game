407e4329c84c072920c6ff9bfd7dc201d6154c84
dbavetta <dbavetta@mail.bradley.edu>
2015-03-30 16:15:34 -0500

    Uploaded Ready For Git

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+	
+	void Update()
+	{
+		Quaternion temp = Camera.main.transform.rotation; //faces the camera
+		transform.rotation = temp;
+	}
+
+}
