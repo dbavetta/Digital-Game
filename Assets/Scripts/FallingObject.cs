e1ca8292e89ea3cd6e2bbbef6ce2c51557664d5c
dbavetta <dbavetta@mail.bradley.edu>
2015-04-06 16:20:48 -0500

    Added falling object (not working yet)

---
+++ Assets/Scripts/FallingObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingObject : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if(other.tag == "Player")
+		{
+			rb.useGravity = true;
+			//rb.isKinematic = false;
+		}
+	}
+}
